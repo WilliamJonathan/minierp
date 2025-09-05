6d32eb4e6752eb576f2d2f3f9c9b4ca6b654ee73
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2025-09-04 23:11:32 -0300

    Update HomePage to display user name in app bar using Consumer

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:minierp/app/pages/login/stores/login_page_store.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,13 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
+      appBar: AppBar(
+        title: Consumer<LoginPageStore>(
+          builder: (context, loginStore, _) {
+            return Text('Home Page - ${loginStore.usuarioModel?.nome ?? ''}');
+          },
+        ),
+      ),
       body: const Center(child: Text('Welcome to the Home Page!')),
     );
   }
